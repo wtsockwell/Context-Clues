5f86cdaa4a38b01020be59606007a070b4008e54
William Taylor Sockwell <wtsockwell@yahoo.com>
2021-02-09 10:54:54 -0600

    Functionality works as intended, going to check before submitting

--- app.js
+++ app.js
@@ -5,21 +5,18 @@ $(document).ready(function () {
     var weapons = ["Rifle", "Knife", "Noose", "Flower Pot", "Pistol", "Inconvenient Torches", "Axe", "Crossbow", "Fire", "Robotic Theme Park Clown", "Log", "Car", "Dem Hands", "Rock", "Pensle", "Rocket Launcher", "Harpoon", "Grenade", "T-Virus", "Chains"]
 
     //Core loop for creating h3 elements as well as implimenting i
-    for (var i = 1; i < 101; i++) {
-        $(`<h3>Acusation ${i}</h3>`).appendTo("body")
+    for (var i = 0; i < 100; i++) {
+        $(`<h3 id=${i}>Acusation ${i+1}</h3>`).appendTo("body")
         //functions for closure and assigning proper values to the postions in the alert
         function crime(){
-            var who
-            var where
-            var which
-            function accuse(who,which,where){
-                alert(`I accuse ${who}, wish the ${which}, in the ${where}`)
+            var who = friends[i%5]
+            var where = locations[i%10]
+            var which = weapons[i%20]
+            function accuse(){
+                alert(`I accuse ${who}, with the ${which}, in the ${where}`)
             }
             return accuse
         }
+        $(`#${i}`).click(crime())
     }
-    $("h3").click(function () {
-        alert(`I accuse ${friends[0]}, with the ${weapons[0]}, in the ${locations[0]}`)
-    })
-
 })
